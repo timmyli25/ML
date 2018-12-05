1f66fb6ca5c608e83fb9d32599f9509e1c58a790
timmyli25 <timmyli530@gmail.com>
2018-12-05 11:53:22 -0600

    changed direction

--- trading/direction.py
+++ trading/direction.py
@@ -57,7 +57,7 @@ total_df['clean_headlines'] = pipeline.news_pipeline(total_df['Headline'])
 
 X_train, X_test, y_train, y_test = train_test_split(total_df['clean_headlines'],total_df['event'])
 
-vectorizer = CountVectorizer(ngram_range=(1, 2), max_features=8000)
+vectorizer = CountVectorizer(ngram_range=(1, 2), max_features=3000)
 train_x_binaries = vectorizer.fit_transform(X_train).toarray()
 test_x_binaries = vectorizer.transform(X_test).toarray()
 clf = BernoulliNB()
@@ -70,13 +70,13 @@ x1 = binaries = vectorizer.transform(x).toarray()
 score2 = clf.score(x1, down_label)
 
 print("Negative Score: {}".format(score2))
-
-
-no_score = pipeline.score_headlines(no_headlines, no_label,vectorizer, clf)
-
+'''
 x = pipeline.news_pipeline(no_headlines)
 x1 = binaries = vectorizer.transform(x).toarray()
 score3 = clf.score(x1, no_label)
 print("No Score: {}".format(score3))
-
-up_score = pipeline.score_headlines(up_headlines, up_label,vectorizer, clf)
+'''
+x = pipeline.news_pipeline(up_headlines)
+x1 = binaries = vectorizer.transform(x).toarray()
+score4 = clf.score(x1, up_label)
+print("Up Score: {}".format(score4))
